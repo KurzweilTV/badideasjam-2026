c9faec3a8e9ddd395ce8e9e0a656540b2293401d
Mike Barr <m.barr@mac.com>
2026-03-04 11:54:34 +0100

    adds ambient and usage sounds

--- props/interactable/oxygen_machine/oxygen_machine.gd
+++ props/interactable/oxygen_machine/oxygen_machine.gd
@@ -1,8 +1,11 @@
 class_name OxygenMachine
 extends Interactable
 
+@onready var use_sound: AudioStreamPlayer3D = $UseSound
+
 
 func _on_interact(_interactor: Player) -> bool:
 	print("Oxygen Refilled - Cooldown: (%s seconds)" % cooldown_time)
+	use_sound.play()
 	_interactor.give_oxygen(100)
 	return true
