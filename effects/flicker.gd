1c6bbabc753e018c054e3e399b5c85bddc36c591
Mike Barr <m.barr@mac.com>
2026-03-09 08:13:16 +0100

    Add airvent prop; update particles, sounds, fonts
    Add a new airvent decoration (model, texture, import settings, Blockbench data and scene) and hook up particles and sound to station power state. Create a reusable QuadMesh particle resource (effects/standard_particle.tres) and refactor damage_particles to use it, disable emission by default, and hide the helper light. Update flicker.gd to connect to StationStatus events, toggle particle emission, light visibility and play a static sound. Replace AudioStreamPlayer usages with a dedicated sound child where appropriate and tween pitch_scale on power-up. Enable MSDF/preload settings for the Kenney Rocket font and switch several SpaceStation Label3D nodes to use that font with adjusted sizes/outlines and reposition airvent instances accordingly.

--- effects/flicker.gd
+++ effects/flicker.gd
@@ -1,10 +1,17 @@
 extends CPUParticles3D
 
 @onready var omni_light_3d: OmniLight3D = $OmniLight3D
+@onready var static_sound: AudioStreamPlayer3D = $StaticSound
 
 func _ready() -> void:
 	_flicker()
+	StationStatus.station_power_change.connect(_on_station_power_up)
 
+func _on_station_power_up(status: bool) -> void:
+	self.emitting = status
+	omni_light_3d.visible = status
+	static_sound.play()
+	
 func _flicker() -> void:
 	while true:
 		var wait_time := randf_range(0.0, 0.4)
--- props/decorations/airvent/airvent.gd
+++ props/decorations/airvent/airvent.gd
@@ -1,16 +1,19 @@
-extends AudioStreamPlayer3D
+extends StaticBody3D
 
 var tween: Tween
+@onready var sound: AudioStreamPlayer3D = $Sound
+@onready var particles: CPUParticles3D = $CPUParticles3D
 
 func _ready() -> void:
 	StationStatus.station_power_change.connect(_on_powerup)
 
-func _on_powerup(_powered: bool) -> void:
+func _on_powerup(powered: bool) -> void:
 	if tween:
 		tween.kill()
-
-	pitch_scale = 0.01
-	play()
+	
+	particles.emitting = powered
+	sound.pitch_scale = 0.01
+	sound.play()
 
 	tween = create_tween()
-	tween.tween_property(self, "pitch_scale", 1.0, 10.0)
+	tween.tween_property(sound, "pitch_scale", 1.0, 10.0)
