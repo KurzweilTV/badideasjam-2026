756c1bba00a3c4339cd434bbb601fe85d2aef907
Mike Barr <m.barr@mac.com>
2026-03-12 17:23:18 +0100

    adds fullscreen button

--- scenes/main_menu/options_menu/options.gd
+++ scenes/main_menu/options_menu/options.gd
@@ -5,6 +5,7 @@ signal options_saved
 
 @onready var hover: AudioStreamPlayer = %Hover
 @onready var click: AudioStreamPlayer = %Click
+@onready var fullscreen: CheckButton = %Fullscreen
 
 func _on_save_close_mouse_entered() -> void:
 	hover.play()
@@ -17,3 +18,9 @@ func _on_tab_container_tab_selected(tab: int) -> void:
 	print("Tab Pressed: %s" % tab)
 	if is_instance_valid(click):
 		click.play()
+
+func _on_fullscreen_toggled(toggled_on: bool) -> void:
+	if toggled_on:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_EXCLUSIVE_FULLSCREEN)
+	else:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
