dfc8bb24a5a154c57695c8ac5bd242e81d8a1fae
Mike Barr <m.barr@mac.com>
2026-03-23 10:18:58 +0100

    new switch texture and model

--- props/interactable/lightsout_switch/lights_out_switch.gd
+++ props/interactable/lightsout_switch/lights_out_switch.gd
@@ -2,6 +2,8 @@ class_name LightsOutSwitch
 extends Interactable
 
 enum switch_positions {A, B, C, D}
+const MATERIAL_1 = preload("uid://bqhlg48dtyhbi") # the emissive texture
+
 
 @export var switch_position: switch_positions
 @export var staring_status: bool
@@ -10,12 +12,33 @@ enum switch_positions {A, B, C, D}
 @onready var collision: CollisionShape3D = $CollisionShape3D
 @onready var switch_sound: AudioStreamPlayer3D = %SwitchSound
 @onready var indicator_light: OmniLight3D = $IndicatorLight
+@onready var sphere: MeshInstance3D = $blockbench_export/sphere
+@onready var sphere_material: StandardMaterial3D = sphere.get_active_material(0).duplicate() as StandardMaterial3D
 
 func _ready() -> void:
 	StationStatus.toggled_powerup_switch.connect(check_switch)
 	StationStatus.switches[switch_position] = staring_status
 	StationStatus.station_power_change.connect(_on_station_powerup)
+	sphere.material_override = sphere_material
 	apply_visual(staring_status)
+
+func apply_visual(status: bool) -> void:
+	if status:
+		anim.play("turn_on")
+		await anim.animation_finished
+		switch_sound.pitch_scale = 0.8
+		switch_sound.play()
+		indicator_light.light_color = Color.SPRING_GREEN
+		sphere_material.emission_enabled = true
+		sphere_material.emission = indicator_light.light_color
+	else:
+		anim.play("turn_off")
+		await anim.animation_finished
+		switch_sound.pitch_scale = 0.7
+		switch_sound.play()
+		indicator_light.light_color = Color.DARK_ORANGE
+		sphere_material.emission_enabled = true
+		sphere_material.emission = indicator_light.light_color
 
 func _on_interact(_interactor: Player) -> bool:
 	StationStatus.toggle_switch(switch_position)
@@ -27,21 +50,6 @@ func check_switch(index: int) -> void:
 		return
 
 	apply_visual(StationStatus.switches[switch_position])
-
-func apply_visual(status: bool) -> void:
-	if status:
-		anim.play("turn_on")
-		await anim.animation_finished
-		switch_sound.pitch_scale = 0.8
-		switch_sound.play()
-		indicator_light.light_color = Color.SPRING_GREEN
-	else:
-		anim.play("turn_off")
-		await anim.animation_finished
-		switch_sound.pitch_scale = 0.7
-	
-		switch_sound.play()
-		indicator_light.light_color = Color.DARK_ORANGE
 
 		
 func _on_station_powerup(_powered) -> void:
