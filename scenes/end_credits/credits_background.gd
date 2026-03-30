d95f9ba405ff6e4c652a798983ccea4348017239
Mike Barr <m.barr@mac.com>
2026-03-30 21:07:20 +0200

    updates credits song

--- scenes/end_credits/credits_background.gd
+++ scenes/end_credits/credits_background.gd
@@ -1,12 +1,10 @@
 extends Node3D
 
-
 func _ready() -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 	SceneManager.set_game_mode(false)
 	credits_complete()
 	
-
 func _on_skip_pressed() -> void:
 	SceneManager.change_scene("uid://btpe1nrboepgg") # Main Menu Scene
 
